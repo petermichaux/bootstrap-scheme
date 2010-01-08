6a7966ccef8f8a229317ea6d6f46e568ebb121d3
Peter Michaux <petermichaux@gmail.com>
2010-01-08 12:54:53 -0800

    changes so value returned by getc is assigned to int

--- scheme.c
+++ scheme.c
@@ -234,19 +234,19 @@ void init(void) {
 
 /***************************** READ ******************************/
 
-char is_delimiter(char c) {
+char is_delimiter(int c) {
     return isspace(c) || c == EOF ||
            c == '('   || c == ')' ||
            c == '"';
 }
 
-char is_initial(char c) {
+char is_initial(int c) {
     return isalpha(c) || c == '*' || c == '/' || c == '>' ||
              c == '<' || c == '=' || c == '?' || c == '!';
 }
 
-char peek(FILE *in) {
-    char c;
+int peek(FILE *in) {
+    int c;
 
     c = getc(in);
     ungetc(c, in);
@@ -254,7 +254,7 @@ char peek(FILE *in) {
 }
 
 void eat_whitespace(FILE *in) {
-    char c;
+    int c;
     
     while ((c = getc(in)) != EOF) {
         if (isspace(c)) {
@@ -266,7 +266,7 @@ void eat_whitespace(FILE *in) {
 }
 
 void eat_expected_string(FILE *in, char *str) {
-    char c;
+    int c;
 
     while (*str != '\0') {
         c = getc(in);
@@ -286,7 +286,7 @@ void peek_expected_delimiter(FILE *in) {
 }
 
 object *read_character(FILE *in) {
-    char c;
+    int c;
 
     c = getc(in);
     switch (c) {
@@ -315,7 +315,7 @@ object *read_character(FILE *in) {
 object *read(FILE *in);
 
 object *read_pair(FILE *in) {
-    char c;
+    int c;
     object *car_obj;
     object *cdr_obj;
     
@@ -355,7 +355,7 @@ object *read_pair(FILE *in) {
 }
 
 object *read(FILE *in) {
-    char c;
+    int c;
     short sign = 1;
     int i;
     long num = 0;
