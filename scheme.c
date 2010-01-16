897b12ab4fa5a8313128cb81a76f3af8986c1635
Peter Michaux <petermichaux@gmail.com>
2010-01-16 13:20:48 -0800

    code formatting

--- scheme.c
+++ scheme.c
@@ -124,48 +124,47 @@ object *read(FILE *in) {
     short sign = 1;
     long num = 0;
 
-        eat_whitespace(in);
+    eat_whitespace(in);
 
-        c = getc(in);    
+    c = getc(in);    
 
-        if (c == '#') { /* read a boolean */
-            c = getc(in);
-            switch (c) {
-                case 't':
-                    return true;
-                case 'f':
-                    return false;
-                default:
-                    fprintf(stderr, "unknown boolean literal\n");
-                    exit(1);
-            }
-        }
-        else if (isdigit(c) || (c == '-' && (isdigit(peek(in))))) {
-            /* read a fixnum */
-            if (c == '-') {
-                sign = -1;
-            }
-            else {
-                ungetc(c, in);
-            }
-            while (isdigit(c = getc(in))) {
-                num = (num * 10) + (c - '0');
-            }
-            num *= sign;
-            if (is_delimiter(c)) {
-                ungetc(c, in);
-                return make_fixnum(num);
-            }
-            else {
-                fprintf(stderr,
-                        "number not followed by delimiter\n");
+    if (c == '#') { /* read a boolean */
+        c = getc(in);
+        switch (c) {
+            case 't':
+                return true;
+            case 'f':
+                return false;
+            default:
+                fprintf(stderr, "unknown boolean literal\n");
                 exit(1);
-            }
+        }
+    }
+    else if (isdigit(c) || (c == '-' && (isdigit(peek(in))))) {
+        /* read a fixnum */
+        if (c == '-') {
+            sign = -1;
         }
         else {
-            fprintf(stderr, "bad input. Unexpected '%c'\n", c);
+            ungetc(c, in);
+        }
+        while (isdigit(c = getc(in))) {
+            num = (num * 10) + (c - '0');
+        }
+        num *= sign;
+        if (is_delimiter(c)) {
+            ungetc(c, in);
+            return make_fixnum(num);
+        }
+        else {
+            fprintf(stderr, "number not followed by delimiter\n");
             exit(1);
         }
+    }
+    else {
+        fprintf(stderr, "bad input. Unexpected '%c'\n", c);
+        exit(1);
+    }
     fprintf(stderr, "read illegal state\n");
     exit(1);
 }
